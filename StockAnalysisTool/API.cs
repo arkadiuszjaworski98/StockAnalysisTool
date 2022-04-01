e75d2d435c482f1c31c8f4c85c5c44bce99eaa6a
arkadiuszjaworski98 <82605116+arkadiuszjaworski98@users.noreply.github.com>
2022-04-01 16:35:49 +0100

    initial commit

---
+++ StockAnalysisTool/API.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace StockAnalysisTool
+{
+    public class API
+	{
+		public API()
+		{
+            // replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+            string QUERY_URL = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo";
+            Uri queryUri = new Uri(QUERY_URL);
+
+            using (WebClient client = new WebClient())
+            {
+
+                dynamic json_data = JsonSerializer.Deserialize<Dictionary<string, dynamic>>(client.DownloadString(queryUri));
+
+                Console.WriteLine("This is the API");
+                foreach (var group in json_data)
+                    Console.WriteLine("Key: {0} Value: {1}", group.Key, group.Value);
+                Console.WriteLine(json_data);
+
+                // -------------------------------------------------------------------------
+
+                // do something with the json_data
+            }
+        }
+	}
+}
---
+++ StockAnalysisTool/Program.cs
@@ -0,0 +1,6 @@
+﻿using static StockAnalysisTool.API;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Stock Analysis Tool");
+Console.WriteLine("Author: Arkadiusz Jaworski");
+object api = new StockAnalysisTool.API();
